76d4787368dfbec4ffcff366ddae1c92905534bc
Batman <cool_omer@hotmail.co.uk>
2021-02-04 18:23:40 +0500

    intial commit

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Routes from '../react-routes/index'
+
+export default (props) => {
+  return(
+    <>
+      {Routes}
+    </>
+  );
+}
---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default ()=> (
+  <div>
+    <h2>Good hogya g</h2>
+    <Link to ="#">Nappo</Link>
+  </div>
+)
