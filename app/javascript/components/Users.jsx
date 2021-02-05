861539cf41119b7f1295acf06be03f47ff2743c0
Batman <cool_omer@hotmail.co.uk>
2021-02-05 19:31:46 +0500

    users index

---
+++ app/javascript/components/Users.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import {Link} from 'react-router-dom'
+
+const Users = function(props){
+  const [stateUsers, setUsers] = useState([]);
+  
+  const url = '/users/index';
+
+  useEffect(()=>{
+    fetch(url).then(res => {
+      if(res.ok){
+        return res.json();
+      }
+      throw new Error("User nahi mile");
+    })
+    .then(res => setUsers(res))
+    .catch(function(){
+      props.history.push('/');
+    })
+  }, [stateUsers.length]);
+
+  return(
+    <div>
+      {stateUsers.map((user) => (
+        <ul>
+          <li key={user.id}>
+            {user.username}
+          </li>
+        </ul>
+      ))}
+    </div>
+  )
+}
+
+export default Users;
--- app/javascript/react-routes/index.jsx
+++ app/javascript/react-routes/index.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from '../components/Home'
 import NewUserForm from '../components/NewUserForm'
+import Users from '../components/Users'
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/new" exact component={NewUserForm} />
+      <Route path="/users" exact component={Users} />
     </Switch>
   </Router>
 );
